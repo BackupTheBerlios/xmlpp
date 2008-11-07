73bc3557436766d1e8e3a0825710c7a154c8fb53
dikobraz <devnull@localhost>
2008-11-07 14:09:16 +0000

    Configuration fixed

---
+++ Configure.cmake
@@ -0,0 +1,21 @@
+SET (CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/CMakeModules;${CMAKE_MODULE_PATH}")
+
+# documentation
+#INCLUDE (Doxy)
+
+# standart
+INCLUDE (StandartIncludes)
+
+# try to find 3rdParty directory
+INCLUDE (Find3rdParty)
+
+# libraries 
+INCLUDE (FindNecessaryLibraries.cmake)
+
+# global dir
+LINK_DIRECTORIES ( ${PROJECT_BINARY_DIR}/lib )
+
+INCLUDE_DIRECTORIES ( 
+	${PROJECT_SOURCE_DIR}/include 
+	${Boost_INCLUDE_DIR}
+)
