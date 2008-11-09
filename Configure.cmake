6f92759ba199ff6ad9172bb6092ea9ff9a549d7c
dikobraz <devnull@localhost>
2008-11-09 16:15:32 +0000

    Little improvements

--- Configure.cmake
+++ Configure.cmake
@@ -4,10 +4,10 @@ SET (CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/CMakeModules;${CMAKE_MODULE_PATH}"
 #INCLUDE (Doxy)
 
 # standart
-INCLUDE (StandartIncludes)
+#INCLUDE (StandartIncludes)
 
 # try to find 3rdParty directory
-INCLUDE (Find3rdParty)
+#INCLUDE (Find3rdParty)
 
 # libraries 
 INCLUDE (FindNecessaryLibraries.cmake)
