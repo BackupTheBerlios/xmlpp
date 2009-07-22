301c509c2997d3af006f91ecdec85f54989894e9
dikobraz <devnull@localhost>
2009-07-22 18:59:32 +0000

    [*] CMakeModules moved to berlios

--- FindNecessaryLibraries.cmake
+++ FindNecessaryLibraries.cmake
@@ -1,7 +1,3 @@
 # boost
-INCLUDE (FindBoost)
-SET (Boost_ADDITIONAL_VERSIONS 1.3*.*)
 FIND_PACKAGE (Boost 1.36.0)
-IF (NOT Boost_FOUND)
-	MESSAGE (FATAL_ERROR "Project ${CMAKE_PROJECT_NAME} couldn't be completely build whithout boost libraries")
-ENDIF (NOT Boost_FOUND)
+
